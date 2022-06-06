b067cb91505441ff1fbaff64a749800439788f7c
khang00 <dlqkhang8@gmail.com>
2022-06-06 14:46:55 +0700

    add some comments and docs

--- src/resolvers/order.ts
+++ src/resolvers/order.ts
@@ -4,6 +4,14 @@ import Orders from "../models/orders";
 import {MutationCreateOrderArgs, QueryGetOrderByIdArgs, QueryGetOrderByUserArgs} from "../generated/graphql";
 import constant from "./constant";
 
+/**
+ * Create an order by its id
+ *
+ * @param _ - not use parameter
+ * @param order - the content of the order
+ * @returns the created order
+ *
+ */
 const createOrder = async <Parent = {}>(_: Parent, {order}: MutationCreateOrderArgs) => {
   const user = await Users.UsersModel.findById(order.user).exec()
   if (!user) {
@@ -23,6 +31,14 @@ const createOrder = async <Parent = {}>(_: Parent, {order}: MutationCreateOrderA
   }
 }
 
+/**
+ * Query every order which belongs to a user
+ *
+ * @param _ - not use parameter
+ * @param user - the id of the user
+ * @returns the order with the predefined id
+ *
+ */
 const getOrderByUser = async <Parent = {}>(_: Parent, {user}: QueryGetOrderByUserArgs) => {
   const orders = await Orders.OrdersModel.find({user: user}).exec()
   if (!orders) {
@@ -43,6 +59,14 @@ const getOrderByUser = async <Parent = {}>(_: Parent, {user}: QueryGetOrderByUse
   })
 }
 
+/**
+ * Query an order by its id
+ *
+ * @param _ - not use parameter
+ * @param _id - the id of the order
+ * @returns the order with the predefined id
+ *
+ */
 const getOrderById = async <Parent = {}>(_: Parent, {_id}: QueryGetOrderByIdArgs) => {
   const order = await Orders.OrdersModel.findById(_id).exec()
   if (!order) {
@@ -61,6 +85,15 @@ const getOrderById = async <Parent = {}>(_: Parent, {_id}: QueryGetOrderByIdArgs
   }
 }
 
+/**
+ * Calculate accrued amount of the order
+ *
+ * @param amount - the original amount the user deposit for the order
+ * @param interestRatePerYear - interest rate per year of the order
+ * @param numberOfMonth - number of month since the order is created
+ * @returns an array of accrued amount respectively to each month
+ *
+ */
 const calculateAccruedAmount = (amount: number, interestRatePerYear: number, numberOfMonth: number) => {
   const interestRatePerMonth = interestRatePerYear / 12
 
@@ -75,8 +108,15 @@ const calculateAccruedAmount = (amount: number, interestRatePerYear: number, num
   }, [])
 }
 
+/**
+ * Calculate accrued amount of the order
+ *
+ * @param amount - the original amount
+ * @param interestRate - interest rate
+ * @returns the calculated interest
+ *
+ */
 const calculateInterest = (amount: number, interestRate: number) => {
-  console.log(amount, interestRate, amount * interestRate)
   return Math.round(amount * interestRate)
 }
 /**
--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -2,6 +2,14 @@ import {UserInputError} from "apollo-server-express";
 import Users from "../models/users";
 import {MutationCreateUserArgs, MutationUpdateUserArgs, QueryUserArgs} from "../generated/graphql";
 
+/**
+ * Create a user
+ *
+ * @param _ - not use parameter
+ * @param user - the user to be created
+ * @returns the created user
+ *
+ */
 const createUser = async <Parent = {}>(_: Parent, {user}: MutationCreateUserArgs) => {
   if (user.phone && !validatePhoneNumber(user.phone)) {
     throw new UserInputError("wrong phone number format" + user.phone)
@@ -10,6 +18,14 @@ const createUser = async <Parent = {}>(_: Parent, {user}: MutationCreateUserArgs
   return await Users.UsersModel.create(user)
 }
 
+/**
+ * Update a user
+ *
+ * @param _ - not use parameter
+ * @param user - the field of a user to be updated
+ * @returns the updated user
+ *
+ */
 const updateUser = async <Parent = {}>(_: Parent, {user}: MutationUpdateUserArgs) => {
   if (user.phone && !validatePhoneNumber(user.phone)) {
     throw new UserInputError("wrong phone number format" + user.phone)
@@ -18,6 +34,14 @@ const updateUser = async <Parent = {}>(_: Parent, {user}: MutationUpdateUserArgs
   return await Users.UsersModel.findByIdAndUpdate(user._id, user).exec()
 }
 
+/**
+ * Query a user by its id
+ *
+ * @param _ - not use parameter
+ * @param _id - the id of a user
+ * @returns the user with the predefined _id
+ *
+ */
 const user = async <Parent = {}>(_: Parent, {_id}: QueryUserArgs) => {
   return await Users.UsersModel.findById(_id).exec()
 }
